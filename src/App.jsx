6705d1c89c8f224c820a1a44482970ad4d06713b
manpoordivyasree26-glitch <manpoordivyasree26@gmail.com>
2026-03-08 16:49:19 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useContext,useMemo } from "react";
+import { NotesContext } from "./context/NotesContext";
+import NotesInput from "./components/NotesInput";
+import NotesList from "./components/NotesList";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+function App(){
+ const{notes}=useContext(NotesContext);
+ const totalNotes=useMemo(()=>{
+  return notes.length;
+ },[notes]);
+ return(
+  <div style={{padding:"20px"}}>
+    <h1>Notes Dashboard</h1>
+    <NotesInput/>
+    <h3>Total Notes:{totalNotes}</h3>
+    <NotesList/>
+  </div>
+ )
+};
+ export default App;
--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -1,9 +1,21 @@
-import {usseontext} from "react";
+import {useContext} from "react";
 import {NotesContext} from "../context/NotesContext";
 import NotesItem from "./NotesItem";
 
 function NotesList(){
     const {notes} =useContext(NotesContext);
+    return(
+        <div>
+            {
+                notes.map((note)=>(
+                   <NotesItem
+                   key={note.id}
+                   note={note}
+                   />
+                ))
+            }
+        </div>
+    )
 
-    
 }
+export default NotesList;
